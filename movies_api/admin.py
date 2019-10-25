d59e3ed03252f8a4e855c7e089f855cb4ee7d81a
witekosz <witekosz@gmail.com>
2019-10-25 20:42:58 +0200

    Register admin models Movie, Comment

--- movies_api/admin.py
+++ movies_api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie, Comment
+
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    readonly_fields = ('id',)
+
+
+class MovieAdmin(admin.ModelAdmin):
+    model = Movie
+    readonly_fields = ('id',)
+    inlines = (CommentInLine,)
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Comment)
