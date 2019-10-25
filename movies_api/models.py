268aece45903e759c4d985870e17b42348dfe2f1
witekosz <witekosz@gmail.com>
2019-10-25 20:32:27 +0200

    Add models Movie, Comment

--- movies_api/models.py
+++ movies_api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeModel(models.Model):
+    """
+    Abstract model for storing instance time data
+    """
+    mod_date = models.DateTimeField(auto_now=True)
+    add_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Movie(models.Model):
+    """
+    Model for storing fetched movies
+    """
+    title = models.CharField(max_length=200)
+    released = models.DateField()
+    runtime = models.PositiveSmallIntegerField()
+
+    genre = models.TextField()
+    director = models.CharField(max_length=200)
+    writer = models.TextField()
+    actors = models.TextField()
+    plot = models.TextField()
+
+    language = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    awards = models.TextField()
+    poster = models.URLField()
+
+
+class Comment(TimeModel):
+    """
+    Model for movie comments
+    """
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.CharField(max_length=50)
+
